e4fbc56d9ca9ac621c86796c7a66cdacdd96b2c6
HDD <151987324+HDD-Team@users.noreply.github.com>
2024-06-10 22:56:52 +0300

    Initial commit

---
+++ metricforge/__init__.py
@@ -0,0 +1,8 @@
+from langchain_core.messages import SystemMessage
+from langchain_core.prompts import HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOllama
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
+from main import script_valid,calculate_metrics
+
